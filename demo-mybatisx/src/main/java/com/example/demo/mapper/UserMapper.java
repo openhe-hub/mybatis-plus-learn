e37d2a9b8225c0b8c585288146e873507b6317b3
openhe <625529334@qq.com>
2023-01-28 16:55:56 +0800

    1-28-upd: mybatisX

--- demo-mybatisx/src/main/java/com/example/demo/mapper/UserMapper.java
+++ demo-mybatisx/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,4 +1,5 @@
 package com.example.demo.mapper;
+import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.example.demo.pojo.User;
@@ -11,8 +12,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @Entity com.example.demo.pojo.User
 */
 public interface UserMapper extends BaseMapper<User> {
-    insertSelective
+    int insertSelective(User user);
 
+    int deleteByIdAndName(@Param("id") Long id, @Param("name") String name);
+
+    int updateAgeAndEmailById(@Param("age") Integer age, @Param("email") String email, @Param("id") Long id);
+
+    List<User> selectAgeAndNameByAgeBetween(@Param("beginAge") Integer beginAge, @Param("endAge") Integer endAge);
+
+    List<User> selectAllByIdOrderByAgeDesc(@Param("id") Long id);
 }
 
 
--- demo-mybatisx/src/test/java/com/example/demo/DemoMybatisxApplicationTests.java
+++ demo-mybatisx/src/test/java/com/example/demo/DemoMybatisxApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoMybatisxApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
